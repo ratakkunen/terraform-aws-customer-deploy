5b86433229c74fb489d6c6ba96d28ebc0c1ce661
Adrian Herrera <simplyadrian@gmail.com>
2019-04-03 20:48:18 +0000

    removing some none DRY things because terraform

--- s3.tf
+++ s3.tf
@@ -1,97 +1,53 @@
-data "template_file" "s3_interfaces_policy" {
-  template = "${file("${path.module}/include/interfaces_bucket_policy.tpl")}"
-
-  vars {
-    account_id                     = "${data.aws_caller_identity.current.account_id}"
-    namespace                      = "${var.namespace}"
-    environment_name               = "${var.environment}"
-    s3_interfaces_bucket           = "${local.s3_interfaces_bucket}"
-  }
-}
-
 module "customer_s3_bucket" {
-  source                 = "git::https://github.com/IDS-Inc/terraform-aws-s3-bucket.git?ref=master"
-  name                   = "${local.s3_interfaces_bucket}"
-  stage                  = "${var.environment}"
-  namespace              = "${var.namespace}"
-  policy                 = "${data.template_file.s3_interfaces_policy.rendered}"
-  force_destroy          = "true"
-  versioning_enabled     = "true"
-  sse_algorithm          = "aws:kms"
-  version_retention_days = "365"
-  log_retention_days     = "365"                                 # 1 year
-}
-
-data "template_file" "s3_origination_policy" {
-  template = "${file("${path.module}/include/origination_bucket_policy.tpl")}"
-
-  vars {
-    account_id                     = "${data.aws_caller_identity.current.account_id}"
-    namespace                      = "${var.namespace}"
-    environment_name               = "${var.environment}"
-    s3_origination_bucket          = "${local.s3_origination_bucket}"
-  }
+  source                       = "git::https://github.com/IDS-Inc/terraform-aws-s3-bucket.git?ref=master"
+  name                         = "${local.s3_interfaces_bucket}"
+  stage                        = "${var.environment}"
+  namespace                    = "${var.namespace}"
+  force_destroy                = "true"
+  versioning_enabled           = "true"
+  sse_algorithm                = "aws:kms"
+  version_retention_days       = "365"
+  log_retention_days           = "365"                                 # 1 year
+  allow_datadog_lambda_logging = "true"
 }
 
 module "origination_s3_bucket" {
-  source                 = "git::https://github.com/IDS-Inc/terraform-aws-s3-bucket.git?ref=master"
-  name                   = "${local.s3_origination_bucket}"
-  stage                  = "${var.environment}"
-  namespace              = "${var.namespace}"
-  policy                 = "${data.template_file.s3_origination_policy.rendered}"
-  force_destroy          = "true"
-  versioning_enabled     = "true"
-  sse_algorithm          = "aws:kms"
-  version_retention_days = "365"
-  log_retention_days     = "365"                                 # 1 year
-}
-
-data "template_file" "s3_insurance_policy" {
-  template = "${file("${path.module}/include/insurance_bucket_policy.tpl")}"
-
-  vars {
-    account_id                     = "${data.aws_caller_identity.current.account_id}"
-    namespace                      = "${var.namespace}"
-    environment_name               = "${var.environment}"
-    s3_insurance_bucket            = "${local.s3_insurance_bucket}"
-  }
+  source                       = "git::https://github.com/IDS-Inc/terraform-aws-s3-bucket.git?ref=master"
+  name                         = "${local.s3_origination_bucket}"
+  stage                        = "${var.environment}"
+  namespace                    = "${var.namespace}"
+  force_destroy                = "true"
+  versioning_enabled           = "true"
+  sse_algorithm                = "aws:kms"
+  version_retention_days       = "365"
+  log_retention_days           = "365"                                 # 1 year
+  allow_datadog_lambda_logging = "true"
 }
 
 module "insurance_s3_bucket" {
-  source                 = "git::https://github.com/IDS-Inc/terraform-aws-s3-bucket.git?ref=master"
-  name                   = "${local.s3_insurance_bucket}"
-  stage                  = "${var.environment}"
-  namespace              = "${var.namespace}"
-  policy                 = "${data.template_file.s3_insurance_policy.rendered}"
-  force_destroy          = "true"
-  versioning_enabled     = "true"
-  sse_algorithm          = "aws:kms"
-  version_retention_days = "365"
-  log_retention_days     = "365"                                 # 1 year
-}
-
-data "template_file" "s3_bank_policy" {
-  template = "${file("${path.module}/include/bank_bucket_policy.tpl")}"
-
-  vars {
-    account_id                     = "${data.aws_caller_identity.current.account_id}"
-    namespace                      = "${var.namespace}"
-    environment_name               = "${var.environment}"
-    s3_bank_bucket                 = "${local.s3_bank_bucket}"
-  }
+  source                       = "git::https://github.com/IDS-Inc/terraform-aws-s3-bucket.git?ref=master"
+  name                         = "${local.s3_insurance_bucket}"
+  stage                        = "${var.environment}"
+  namespace                    = "${var.namespace}"
+  force_destroy                = "true"
+  versioning_enabled           = "true"
+  sse_algorithm                = "aws:kms"
+  version_retention_days       = "365"
+  log_retention_days           = "365"                                 # 1 year
+  allow_datadog_lambda_logging = "true"
 }
 
 module "bank_s3_bucket" {
-  source                 = "git::https://github.com/IDS-Inc/terraform-aws-s3-bucket.git?ref=master"
-  name                   = "${local.s3_bank_bucket}"
-  stage                  = "${var.environment}"
-  namespace              = "${var.namespace}"
-  policy                 = "${data.template_file.s3_bank_policy.rendered}"
-  force_destroy          = "true"
-  versioning_enabled     = "true"
-  sse_algorithm          = "aws:kms"
-  version_retention_days = "365"
-  log_retention_days     = "365"                                 # 1 year
+  source                       = "git::https://github.com/IDS-Inc/terraform-aws-s3-bucket.git?ref=master"
+  name                         = "${local.s3_bank_bucket}"
+  stage                        = "${var.environment}"
+  namespace                    = "${var.namespace}"
+  force_destroy                = "true"
+  versioning_enabled           = "true"
+  sse_algorithm                = "aws:kms"
+  version_retention_days       = "365"
+  log_retention_days           = "365"                                 # 1 year
+  allow_datadog_lambda_logging = "true"
 }
 
 resource "aws_s3_bucket_object" "upload_interfaces_default_object" {
